7dd66926c58f4ee3d81a9fcb08199aca7a547a8b
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-01 07:00:42 +0600

    add validation of password

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import auth from './firebase.init';
 
 
 function App() {
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState('')
   const [users, setUsers] = useState({})
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -22,6 +24,19 @@ function App() {
   }
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return
+    }
+    if (!/(?=.*[a-zA-Z >>!#$%&? "<<])[a-zA-Z0-9 >>!#$%&?<< ]/.test(password)) {
+      setError('Please add at least on special charecter')
+      return;
+    }
+    setError('')
+    setValidated(true);
+
     createUserWithEmailAndPassword(auth, email, password)
       .then(result => {
         const user = result.user;
@@ -36,18 +51,27 @@ function App() {
     <div className=''>
       <h2 className='text-center  my-5'>Firebase authentication and validation</h2>
       <div className="w-50 mx-auto">
-        <Form onSubmit={handleSubmit}>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control onBlur={handleEmail} type="email" placeholder="Enter email" required />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control onBlur={handlePassword} type="password" placeholder="Password" required />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid password.
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
           </Form.Group>
+          <Form.Control.Feedback>
+            {error}
+          </Form.Control.Feedback>
           <Button variant="primary" type="submit">
             Submit
           </Button>
